01e8d1a6fc084ab4514bb661b760fece79c8dab3
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-22 14:11:04 +0100

    Implement the Employee entity

---
+++ src/main/java/com/Authentification/System/Entity/Employee.java
@@ -0,0 +1,23 @@
+package com.Authentification.System.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employees", schema = "springboot_schema")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String role;
+}
